b57d2575cc34776b92f75befd8cb298a962b4e40
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-06-20 18:31:19 +0000

    Operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,36 @@
+// ********* OPERATIONS ***********
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%2);
+
+
+// let str1 = "Vivek"
+// let str2 = "Hitesh"
+
+// let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2) 
+// console.log(1 + 2 + "2")
+
+// console.log(true);
+// console.log(+true) // ==> 1
+
+// console.log(+"") // ==> 0
+ 
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+// let gameCounter = 100
+// gameCounter++
+// console.log(gameCounter);
+
