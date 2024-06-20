9b83047142fbd04b12a50faf89f5cc520558aad0
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-06-20 17:55:42 +0000

    datatypes

--- 01_basics/datatypes2.js
+++ 01_basics/datatypes2.js
@@ -6,5 +6,21 @@ console.log(3
     +3);    //Code readability should be high
 
     console.log("Hitesh ");
+ 
+
+    //Datatypes 
+    //Number => 2 to power 53
+    //bigint
+    //String => ""
+    //boolean => true/false
+    //null => Stand alone value
+    //undefined => 
+    //symbol => uniques
+    //object 
+
+    console.log(typeof "Vivek");
+    console.log(typeof null);
+    console.log(typeof undefined);
+
 
     
