c09a68939986042e50d84a5d94bdfab1225961fe
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-06-21 06:15:40 +0000

    datatypes_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,37 @@
+//In JavaScript there are tow types of Datatypes
+// #1. Primitive :- 
+//     7 Types : String , Number,  Boolean, null, undefined, Symbol,Bigint
+ 
+
+const score = 100
+const scoreValue = 100.3 
+
+const isLoggedin = false 
+
+const outsideTemp = null
+
+let useEmail;
+  
+const id = Symbol('123')
+const anotherId = symbol('123')
+
+console.log(id === anotherId);
+
+const bigInt = 957648943658464n
+
+// #2. Reference(Non Premitive) :-
+//    Array, Objects, Function
+
+const heros = ["Shaktiman", "Naagraj", "Doga"]
+
+let myObj = {
+    name:"vivek",
+    age:21
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+
