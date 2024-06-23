c8988336c5bbac5661a12bc59e120791301b0a52
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-06-23 12:57:01 +0000

    NUmbers and Maths

---
+++ 01_basics/numbers.js
@@ -0,0 +1,29 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8944
+console.log(otherNumber.toPrecision(3));
+
+const huundreds = 1000000000
+console.log(huundreds.toLocaleString('en-IN'));
+console.log(huundreds.toLocaleString());
+console.log("'****************************'");
+
+
+
+// ******* MATHS *******
+console.log(MAth);
+console.log(MAth.abs(4));
+console.log(Math.round(4.3));
+console.log(Math.floor(4.9));
+console.log(Math.ceil(4.1));
+
+
+
+
