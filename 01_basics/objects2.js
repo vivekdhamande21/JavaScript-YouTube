ccc065903ea5e7cc5289765890df22dcdc8bb113
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-06-24 19:04:35 +0000

    object2.js

---
+++ 01_basics/objects2.js
@@ -0,0 +1,39 @@
+// //const tinderUser = new Object()
+
+// const tinderUser = {}
+
+// tinderUser.id = "23abc"
+// tinderUser.name = "sammy"
+// tinderUser.loggedIn = false
+
+// // console.log(tinderUser);
+
+// const regularUser = {
+//     email: "somenath@234.com",
+//         userfullName: {
+//                 fullName: {
+//                     firstName:"Vivek",
+//                     lastName:"Dhamande"
+//              }
+//         }
+// }
+
+// console.log(regularUser.userfullName.fullName.firstName);
+
+// const obj1 = {1:"a", 2: "b"}
+// const obj2 = {3:"a", 4: "b"}
+// const obj4 = {5:"a", 6: "b"}
+
+
+// // const obj3 = {obj1,obj2}
+
+
+// // const obj3 = Object.assign({},obj1,obj2,obj4)
+// // console.log(obj3);
+
+// const obj3 = {...obj1,...obj2}
+
+
+// const user = {
+
+// }
