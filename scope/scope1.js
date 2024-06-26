8c7c67d24d62c46047a7a2e92dba296215ae0432
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-06-26 18:33:23 +0000

    Scope

--- scope/scope1.js
+++ scope/scope1.js
@@ -1,10 +1,44 @@
-var c = 300
-let a = 200
+// var c = 300
+// let a = 200
+// if(true){
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one(){
+//     const username = "Vivek"
+//         function two(){
+//             const website = "youtube"
+//             console.log(username);
+//         }
+//         //console.log(website); 
+
+//         two()
+// }
+// one()
+
 if(true){
-    let a = 10
-    const b = 20
-    var c = 30
+    const username = "Vivek"
+    if(username === "Vivek"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
 }
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(username);
+
+//****************Intresting*************************
+
+console.log(addOne(5));
+function addOne(num){
+    return num + 1; 
+}
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
