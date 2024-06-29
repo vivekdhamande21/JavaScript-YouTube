47c48b4769eba991de64a592853ee635e6035ed2
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-06-29 10:45:42 +0000

    Control Flow

---
+++ 04_controlflow/one.js
@@ -0,0 +1,30 @@
+//if 
+
+const isuserLoggedIn = true
+const temp = 40
+if(temp < 50){
+    console.log("Less then 50");
+}
+
+
+if(2 === "2"){
+    console.log("executed");
+}
+// <, >, >=, <=, ==, !=, ===, !==,
+
+
+//Nullish Coalescing Operator
+
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 20
+console.log(val1);
+
+
+// Ternaiary Opreator
+// condition ? true : false
+
+const ice = 100
+ice >=80 ? console.log("less tham 80") : console.log("moe than 80"); ;
